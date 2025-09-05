aedd4cb48bda65733a5b777472495cde2edf4616
n3mr1d <n3mr1d@gmail.com>
2025-09-06 06:20:53 +0700

    first commit

--- github/LibCMSLaravel/resources/views/components/app-logo.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="flex flex-col items-center" >
-<div class="ms-1 mb-3 grid flex-1 text-start text-xl">
-    <span class="mb-0.5 truncate  text-black dark:text-white leading-tight font-semibold">{{ config('app.name') }} {{ date('Y') }}</span>
-</div>
-<div class="flex transparant aspect-square size-100 items-center justify-center  text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-
-</div>
--- github/LibCMSLaravel/resources/views/welcome.blade.php
+++
@@ -1,4 +0,0 @@
-<x-main-head>
-    <a wire:navigate href="{{ route('login') }}">Login</a>
-    <a wire:navigate href="{{ route('register') }}">Register</a>
-</x-main-head>
